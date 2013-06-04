9b9fd7bb9fa0957bb5170d98352a23bd98aba36d
Kiyoshi Ikehara <kiyoshi.ikehara@gree.net>
2013-06-04 14:19:50 +0900

    Imprement get operation of memcached-proxy

--- main/memcached-proxy.hs
+++ main/memcached-proxy.hs
@@ -12,11 +12,15 @@ import Data.Conduit.Network hiding (runTCPClient)
 import Data.Conduit.Network.Internal
 import Data.Conduit.Memcache
 import Network.Socket
+import qualified Data.ByteString.Char8 as BS
+import Control.Concurrent hiding (yield)
 
+import Network.Memcache.Op
 import Network.Memcache.Response
 
 main :: IO ()
 main = do
+  setNumCapabilities 4
   let sSettings0 = (serverSettings 13302 HostAny) :: ServerSettings IO
       sSettings = sSettings0  { serverAfterBind = (\s -> setSocketOption s NoDelay 1) }
   runTCPServer sSettings $ \appData -> do
@@ -26,23 +30,47 @@ proxyLoop :: AppData IO -> AppData IO -> IO ()
 proxyLoop serverData clientData = do
   (serverSource, ()) <- (appSource serverData $= getOpText) $$+ return ()
   (clientSource, ()) <- (appSource clientData $= getResponseText) $$+ return ()
-  loop serverSource (appSink serverData) clientSource (appSink clientData)
+  proxyLoop' serverSource (appSink serverData) clientSource (appSink clientData)
+
+proxyLoop' :: (MonadIO m) =>
+              ResumableSource m (Either String Op)
+              -> Sink BS.ByteString m a1
+              -> ResumableSource m Response
+              -> Sink BS.ByteString m a
+              -> m ()
+proxyLoop' serverSource serverSink clientSource clientSink = do
+  (serverSource', mop) <- serverSource $$++ await
+  case mop of
+    Nothing -> return ()
+    Just (Left msg) -> do
+      yield Error $$ (putResponseText =$ serverSink)
+      proxyLoop' serverSource' serverSink clientSource clientSink
+    Just (Right op) -> case op of
+      GetOp keys -> do
+        yield op $$ (putOpText =$ clientSink)
+        clientSource' <- proxyGet clientSource
+        proxyLoop' serverSource' serverSink clientSource' clientSink
+      _ -> do
+        yield op $$ (putOpText =$ clientSink)
+        (clientSource', mresp) <- clientSource $$++ await
+        case mresp of
+          Nothing -> return ()
+          Just resp -> do
+            yield resp $$ (putResponseText =$ serverSink)
+            proxyLoop' serverSource' serverSink clientSource' clientSink
   where
-    loop serverSource serverSink clientSource clientSink = do
-      (serverSource', mop) <- serverSource $$++ await
-      case mop of
-        Nothing -> return ()
-        Just (Left msg) -> do
-          yield Error $$ (putResponseText =$ serverSink)
-          loop serverSource' serverSink clientSource clientSink
-        Just (Right op) -> do
-          yield op $$ (putOpText =$ clientSink)
-          (clientSource', mresp) <- clientSource $$++ await
-          case mresp of
-            Nothing -> return ()
-            Just resp -> do
-              yield resp $$ (putResponseText =$ serverSink)
-              loop serverSource' serverSink clientSource' clientSink
+    proxyGet clientSource = do
+      (clientSource', mresp) <- clientSource $$++ await
+      case mresp of
+        Nothing -> return (clientSource')
+        Just resp -> case resp of
+          Value {} -> do
+            yield resp $$ (putResponseText =$ serverSink)
+            proxyGet clientSource'
+          End -> do
+            yield End $$ (putResponseText =$ serverSink)
+            return (clientSource')
+
 
 runTCPClient :: (MonadIO m, MonadBaseControl IO m) => ClientSettings m -> Application m -> m ()
 runTCPClient settings app = control $ \run -> bracket
--- src/Data/Conduit/Memcache.hs
+++ src/Data/Conduit/Memcache.hs
@@ -8,17 +8,17 @@ import Control.Applicative
 import Data.Attoparsec.ByteString.Char8
 -- import Data.Attoparsec.Binary
 import qualified Data.Attoparsec.ByteString.Lazy as AL
-import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString.Char8 as BS
 import Data.Conduit
 import Data.Conduit.Attoparsec
 
 import Network.Memcache.Op
 import Network.Memcache.Response
 
-getOpText :: (MonadIO m, MonadThrow m) => ConduitM C.ByteString (Either String Op) m ()
+getOpText :: (MonadIO m, MonadThrow m) => ConduitM BS.ByteString (Either String Op) m ()
 getOpText = getOpText' =$= removePR
 
-getOpText' :: (MonadIO m, MonadThrow m) => ConduitM C.ByteString (PositionRange, Either String Op) m ()
+getOpText' :: (MonadIO m, MonadThrow m) => ConduitM BS.ByteString (PositionRange, Either String Op) m ()
 getOpText' = conduitParser command
   where
     command :: Parser (Either String Op)
@@ -46,33 +46,35 @@ removePR = do
       yield r
       removePR
 
-getResponseText :: (MonadIO m, MonadThrow m) => ConduitM C.ByteString Response m ()
+getResponseText :: (MonadIO m, MonadThrow m) => ConduitM BS.ByteString Response m ()
 getResponseText = getResponseText' =$= removePR
 
-getResponseText' :: (MonadIO m, MonadThrow m) => ConduitM C.ByteString (PositionRange, Response) m ()
+getResponseText' :: (MonadIO m, MonadThrow m) => ConduitM BS.ByteString (PositionRange, Response) m ()
 getResponseText' = conduitParser responseParser
 
-putResponseText :: MonadIO m => ConduitM Response C.ByteString m ()
+putResponseText :: MonadIO m => ConduitM Response BS.ByteString m ()
 putResponseText = do
   mResp <- await
   case mResp of
     Nothing -> return ()
     Just resp -> do
       mapM_ yield $ Network.Memcache.Response.toChunks resp
+      -- yield $ BS.concat $ Network.Memcache.Response.toChunks resp
       putResponseText
 
-putOpText :: MonadIO m => ConduitM Op C.ByteString m ()
+putOpText :: MonadIO m => ConduitM Op BS.ByteString m ()
 putOpText = do
   mOp <- await
   case mOp of
     Nothing -> return ()
     Just op -> do
       mapM_ yield $ Network.Memcache.Op.toChunks op
+      -- yield $ BS.concat $ Network.Memcache.Op.toChunks op
       putOpText
 
 ----
 
-endline :: Parser C.ByteString
+endline :: Parser BS.ByteString
 endline = try (string "\r\n") <|> string "\n" <|> string "\r"
 
 skipSpace' :: Parser ()
