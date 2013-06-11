f9ee69285a2462d4d5796acffc53c2747319820a
Kiyoshi Ikehara <kiyoshi.ikehara@gree.net>
2013-06-11 11:12:42 +0900

    Remove intermediate conduits

--- src/Data/Conduit/Memcache.hs
+++ src/Data/Conduit/Memcache.hs
@@ -4,11 +4,9 @@ module Data.Conduit.Memcache (getOpText, getResponseText, putOpText, putResponse
 
 import Prelude hiding (takeWhile)
 import Control.Monad.Trans
--- import Control.Monad.IO.Class
 import Control.Applicative
 import qualified Data.Attoparsec.ByteString as AB
 import Data.Attoparsec.ByteString.Char8
--- import Data.Attoparsec.Binary
 import qualified Data.Attoparsec.ByteString.Lazy as AL
 import qualified Data.ByteString.Char8 as BS
 import Data.Conduit
@@ -19,16 +17,36 @@ import Network.Memcache.Op
 import Network.Memcache.Response
 
 getOpText :: (MonadIO m, MonadThrow m) => ConduitM BS.ByteString (Either BS.ByteString Op) m ()
-getOpText = getOpText' =$= removePR
-
-getOpText' :: (MonadIO m, MonadThrow m) => ConduitM BS.ByteString (PositionRange, Either BS.ByteString Op) m ()
-getOpText' = conduitParser command
+getOpText = conduitParser command =$= removePR
   where
     command :: Parser (Either BS.ByteString Op)
     command = try (Right <$> opParser) <|> (Left <$> (AB.takeWhile (\c -> c /= 10 && c /= 13) <* endline))
 
     endline :: Parser BS.ByteString
     endline = try (string "\r\n") <|> string "\n" <|> string "\r"
+
+getResponseText :: (MonadIO m, MonadThrow m) => ConduitM BS.ByteString Response m ()
+getResponseText = conduitParser responseParser =$= removePR
+
+putResponseText :: MonadIO m => ConduitM Response BS.ByteString m ()
+putResponseText = do
+  mResp <- await
+  case mResp of
+    Nothing -> return ()
+    Just resp -> do
+      yield $ BS.concat $ toChunks resp
+      putResponseText
+
+putOpText :: MonadIO m => ConduitM Op BS.ByteString m ()
+putOpText = do
+  mOp <- await
+  case mOp of
+    Nothing -> return ()
+    Just op -> do
+      yield $ BS.concat $ toChunks op
+      putOpText
+
+----------------------------------------------------------------
 
 removePR :: (MonadIO m, MonadThrow m) => ConduitM (PositionRange, t) t m ()
 removePR = do
@@ -38,30 +56,3 @@ removePR = do
     Just (_, r) -> do
       yield r
       removePR
-
-getResponseText :: (MonadIO m, MonadThrow m) => ConduitM BS.ByteString Response m ()
-getResponseText = getResponseText' =$= removePR
-
-getResponseText' :: (MonadIO m, MonadThrow m) => ConduitM BS.ByteString (PositionRange, Response) m ()
-getResponseText' = conduitParser responseParser
-
-putResponseText :: MonadIO m => ConduitM Response BS.ByteString m ()
-putResponseText = do
-  mResp <- await
-  case mResp of
-    Nothing -> return ()
-    Just resp -> do
-      mapM_ yield $ toChunks resp
-      -- yield $ BS.concat $ Network.Memcache.Response.toChunks resp
-      putResponseText
-
-putOpText :: MonadIO m => ConduitM Op BS.ByteString m ()
-putOpText = do
-  mOp <- await
-  case mOp of
-    Nothing -> return ()
-    Just op -> do
-      mapM_ yield $ toChunks op
-      -- yield $ BS.concat $ Network.Memcache.Op.toChunks op
-      putOpText
-
